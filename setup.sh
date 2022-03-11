a7aabe989484d1602799483e3f7ac264a2551973
frani <franinnocenti@gmail.com>
2022-03-11 16:04:54 -0300

    fix: set right logic to break while True loop

--- setup.sh
+++ setup.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-echo "Which OS want to setup? [mac/raspberry]"
-echo ""
 while true; do
-    
     echo "Which OS want to setup? [ mac / raspberry ]"
     echo "default is mac"
     read READ_OS 
     [[ $READ_OS == "" ]] && READ_OS="mac"
+    [[ $READ_OS == "mac" || $READ_OS == "raspberry" ]] && break
 done
 
 echo ""
-echo "Setupping $READ_OS"
+echo "Setting $READ_OS up"
 git clone https://github.com/frani/setup frani_setup_os
 cd frani_setup_os
 chmod +x $(echo "setup_$READ_OS.sh")
