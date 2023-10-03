b92ba8facdca184a22baa62c922f4749554994f0
frani <franinnocenti@gmail.com>
2023-10-03 11:39:34 -0300

    chore: add enable sudo touchid

---
+++ enable_touchid_sudo.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Enable Touch ID for sudo
+
+if ! sudo -v; then
+    echo "🚫 User does not have sudo permissions"
+    exit 1
+fi
+
+if grep -q pam_tid.so /etc/pam.d/sudo; then
+    echo "👌 Touch ID sudo already set up"
+else
+    sudo gsed -i '2i\'"auth       sufficient     pam_tid.so" /etc/pam.d/sudo
+    echo "👍 Touch ID sudo added"
+fi
